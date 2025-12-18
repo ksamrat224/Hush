11cfc9660d971fc1fd8a879f94eebf2c0c538999
Samrat <ksamrat224@gmail.com>
2025-12-18 18:54:57 +0545

    feat: add socket.io real-time communication support

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,16 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 
+const io = new Server(server);
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
+
+io.on("connection", (socket) => {
+  console.log("a user connected",socket.id);
+});
+
 
 server.listen(4600, () => {
   console.log("server running at http://localhost:4600");
